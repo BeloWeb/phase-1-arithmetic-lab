4b8fa4b62e6959bf42e2f0783174ff6cf31b1b7c
unknown <belonyfdd90@gmail.com>
2025-08-09 11:32:51 -0700

    BeloWeb-Arithmetique-lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
-// Write your code here
+// Définition des variables globales
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+// Génère un nombre aléatoire entre 1 et 1000
+const random = Math.floor(Math.random() * 1000) + 1;
+// Définition des variables pour le modulo
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+// Définition du tableau pour max
+const numbers = [10, 5, 20, 15];
+const max = Math.max(...numbers);
